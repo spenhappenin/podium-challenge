044f1d1be47fae94c9ab001217e085b9083d000d
Spencer Richards <spencer.richards7@gmail.com>
2018-01-18 10:28:53 -0700

    fetch and display review bodies

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,24 +1,57 @@
 import React from 'react';
+import ReviewDescription from './ReviewDescription';
 import PropTypes from 'prop-types';
-import { Card } from 'semantic-ui-react';
+// import axios from 'axios';
+import { Button, Card } from 'semantic-ui-react';
 
-const Review = ({ review: {author, rating, publish_date} }) => (
-  <Card>
-    <Card.Content>
-      <Card.Header>
-        { author }
-      </Card.Header>
-      <Card.Meta>
-        <span className='date'>
-          Rating: { rating }
-        </span>
-      </Card.Meta>
-      <Card.Description>
-        { publish_date }
-      </Card.Description>
-    </Card.Content>
-  </Card>
-)
+class Review extends React.Component {
+  state = { show: false };
+
+  showDetails = (reviewId) => {
+    // const { show } = this.state;
+    // if(show) {
+    //   axios.get(`http://shakespeare.podium.co/api/reviews/${reviewId}`, { headers: { 'Authorization': 'koOheljmQX' } })
+    //     .then( res => {
+    //       console.log(res)
+    //     })
+    //     .catch( res => {
+    //       console.log(res)
+    //     })
+    // }
+    return <p>Working!</p>
+  }
+
+  toggleShow = () => this.setState({ show: !this.state.show });
+
+  render() {
+    const { show } = this.state;
+    const { review: { author, id, rating, publish_date } } = this.props;
+    return(
+      <Card>
+        <Card.Content>
+          <Card.Header>
+            { author }
+          </Card.Header>
+          <Card.Meta>
+            <span className='date'>
+              Rating: { rating }
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            { publish_date }
+          </Card.Description>
+          <Card.Content extra>
+            <Button basic onClick={this.toggleShow}>
+              { show ? 'Close Details' : 'Show Details' }
+            </Button>
+            {/* { show ? () => this.showDetails(id) : null } */}
+            { show ? <ReviewDescription reviewId={id} /> : null }
+          </Card.Content>
+        </Card.Content>
+      </Card>
+    )
+  }
+}
 
 Review.propTypes = {
   review: PropTypes.object,
---
+++ src/components/ReviewDescription.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Header } from 'semantic-ui-react';
+
+class ReviewDescription extends React.Component {
+  state = { description: '' };
+
+  componentDidMount() {
+    const { reviewId } = this.props;
+    axios.get(`http://shakespeare.podium.co/api/reviews/${reviewId}`, { headers: { 'Authorization': 'koOheljmQX' } })
+      .then( res => {
+        this.setState({ description: res.data.data.body });
+      })
+      .catch(res => {
+        console.log(res);
+      })
+  }
+
+  render() {
+    const { description } = this.state;
+    if(description)
+      return <Header as='h5'>{description}</Header>
+    return null
+  }
+}
+
+ReviewDescription.propTypes = {
+  reviewId: PropTypes.string
+}
+
+export default ReviewDescription;
